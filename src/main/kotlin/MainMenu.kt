cd99cfa06eaa9e21f29c25d8c42b4e62b5cbc8f4
Chistyakov Alexey <chskela@gmail.com>
2022-09-28 17:46:31 +0300

    Update MainMenu.kt

--- src/main/kotlin/MainMenu.kt
+++ src/main/kotlin/MainMenu.kt
@@ -1,9 +1,11 @@
-fun mainMenu(): String {
-    return """
-        Welcome to Console Game v1.0. Please make your choice:
-        1: Start new game
-        2: Options
-        3: Credits
-        4: Exit
+class MainMenu {
+    fun showMenu(): String {
+        return """
+        Welcome to Console Game v1.0. Please make your choice and press Enter:
+          1: Start new game
+          2: Options
+          3: Credits
+          4: Exit
     """.trimIndent()
+    }
 }
