d4be7eeedbfc91076338c8312ab0bfe19a735826
Chistyakov Alexey <chskela@gmail.com>
2022-09-28 21:38:27 +0300

    Update Main Menu. Closes #6

--- src/main/kotlin/menu/MainMenu.kt
+++ src/main/kotlin/menu/MainMenu.kt
@@ -1,9 +1,40 @@
 package menu
 
-class MainMenu {
-    fun showMenu() {
+import Game
+
+class MainMenu(
+    private val optionsMenu: OptionsMenu,
+    private val creditsMenu: CreditsMenu,
+    private val game: Game
+) {
+    private var isExit: Boolean = false
+
+    fun loop() {
+        do {
+            showMenu()
+
+            when (readLine()?.toInt() ?: 0) {
+                1 -> game.startNewGame()
+
+                2 -> optionsMenu.loop()
+
+                3 -> creditsMenu.showMenu()
+
+                4 -> {
+                    isExit = true
+                }
+
+                else -> {
+                    println("Command not recognized! Please try again")
+                }
+            }
+        } while (!isExit)
+    }
+
+    private fun showMenu() {
         println(
             """
+        <====================================================================>
         Welcome to Console Game v1.0. Please make your choice and press Enter:
           1: Start new game
           2: Options
