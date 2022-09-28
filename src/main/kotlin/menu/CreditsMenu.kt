6fa11a656c43ae4030ac75ac214c4de59c5e1eb8
Chistyakov Alexey <chskela@gmail.com>
2022-09-28 17:58:48 +0300

    Added Credits Menu. Closes #4

---
+++ src/main/kotlin/menu/CreditsMenu.kt
@@ -0,0 +1,11 @@
+package menu
+
+class CreditsMenu {
+    fun showMenu(): String {
+        return """
+        Created by Chistyakov Aleksey
+        version 1.0, last modified 28.09.2022
+        contact me at chskela@gmail.com
+    """.trimIndent()
+    }
+}
