c910ebcb273eb5b8930601eef8176f2396e5e26b
Chistyakov Alexey <chskela@gmail.com>
2022-09-28 17:31:47 +0300

    Added Main menu. Closes #1

---
+++ src/main/kotlin/MainMenu.kt
@@ -0,0 +1,9 @@
+fun mainMenu(): String {
+    return """
+        Welcome to Console Game v1.0. Please make your choice:
+        1: Start new game
+        2: Options
+        3: Credits
+        4: Exit
+    """.trimIndent()
+}
