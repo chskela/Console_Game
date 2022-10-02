d58e9ac0f92719ca448302a5533f1cf101cf0449
Chistyakov Alexey <chskela@gmail.com>
2022-10-02 21:14:02 +0300

    Add constants. Closes #15

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,5 @@
+
+const val LEFT = "a"
+const val RIGHT = "d"
+const val UP = "w"
+const val DOWN = "s"
--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -118,9 +118,3 @@ class Game(
         }
     }
 }
-
-
-const val LEFT = "a"
-const val RIGHT = "d"
-const val UP = "w"
-const val DOWN = "s"
