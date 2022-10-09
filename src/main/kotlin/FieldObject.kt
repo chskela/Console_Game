d18cf2141c35546c6f975aba5f1457ce1355dcdd
Chistyakov Alexey <chskela@gmail.com>
2022-10-09 17:12:28 +0300

    Updated FieldObject - add color for symbol

--- src/main/kotlin/FieldObject.kt
+++ src/main/kotlin/FieldObject.kt
@@ -1,11 +1,11 @@
 sealed class FieldObject(val symbol: String) {
     object Empty : FieldObject(" . ")
 
-    data class Enemy(val coordinate: Pair<Int, Int>) : FieldObject(" % ")
+    data class Enemy(val coordinate: Pair<Int, Int>) : FieldObject("\u001B[31m % \u001B[0m")
 
-    data class Player(val coordinate: Pair<Int, Int>) : FieldObject(" @ ")
+    data class Player(val coordinate: Pair<Int, Int>) : FieldObject("\u001b[1m \u001b[34m @ \u001b[0m")
 
-    data class Flower(val amount: Int) : FieldObject(" $amount ")
+    data class Flower(val amount: Int) : FieldObject("\u001b[33m $amount \u001B[0m")
 }
 
 fun FieldObject.Player.makeMove(coordinate: Pair<Int, Int>): FieldObject.Player = this.copy(coordinate = coordinate)
